37794754cc9dcd3e82aa236063e754dc69e33d60
danhbh03 <148253811+danhbh03@users.noreply.github.com>
2024-01-01 08:39:56 +0700

    Sửa lỗi hoàn thành Level
    đã có sự cập nhật trong bảng điểm Headshot và bodyshot nhưng vẫn chưa thể cập nhật được khi giết đối phương cuối cùng

--- city-snipper-master/city-snipper-client/Assets/OverAll/ScoreScript.cs
+++ city-snipper-master/city-snipper-client/Assets/OverAll/ScoreScript.cs
@@ -6,14 +6,13 @@ using UnityEngine.UI;
 public class ScoreScript : MonoBehaviour
 {
 
-   // public GameObject Enemy;
+    // public GameObject Enemy;
     public GameObject[] scoreobjects;
     private int curentindex = 0;
     public int score = 0;
     public int currentscore;
 
     public Text textScore;
-
     public GameObject HeadShot;
     public static ScoreScript instance;
     // Start is called before the first frame update
@@ -40,7 +39,9 @@ public class ScoreScript : MonoBehaviour
     public void HeadShots()
     {
         HeadShot.SetActive(true);
+        Statistics.instance.Headshots();
     }
+
 
     public void NewRandomObject()
     {
@@ -73,7 +74,7 @@ public class ScoreScript : MonoBehaviour
 
         currentscore = 0;
 
-       
+
     }
 
 
